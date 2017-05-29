c7c8b572d9387a17e75df68e75aed1aaa59496d1
ArtemAlieinikov <artemalieinikov@gmail.com>
2017-05-30 02:23:45 +0300

    Added new methods for working with animals sequences.

--- Binary-Studio-Zoo-Task/Interfaces/IZooRepository.cs
+++ Binary-Studio-Zoo-Task/Interfaces/IZooRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 using Binary_Studio_Zoo_Task.Classes.Animals;
 using Binary_Studio_Zoo_Task.Enums;
 
@@ -11,12 +12,25 @@ namespace Binary_Studio_Zoo_Task.Interfaces
     public interface IZooRepository
     {
         void AddAnimal(Animal newAnimal);
-        Animal GetRandomAnimal();
-        Animal GetAnimalByName(string animalName);
         bool DeleteAnimalByName(string animalName);
+        double GetAvarageHealth();
+
+        Animal GetRandomAnimal();
+        Animal GetAnimal(string animalName);
+        Animal GetAnimal(AnimalType animalType, string animalName);
+
+        IEnumerable<IGrouping<AnimalType, Animal>> GetAnimalGroups();
+
+        Dictionary<AnimalType, Animal> GetHealthiestAnimalOnGroup();
+        Dictionary<AnimalType, int> GetNumberOfDethInGroup();
+
+        List<string> GetNamesOfHangryAnimals();
 
         List<Animal> GetAllAnimals();
-        List<Animal> GetAnimalsByType(AnimalType animalType);
-        List<Animal> GetAnimalByState(AnimalState animalState);
+        List<Animal> GetAnimalsWithMaxAndMinHealth();
+        List<Animal> GetAnimals(AnimalType animalType);
+        List<Animal> GetAnimals(AnimalState animalState);
+        List<Animal> GetWolfsAndBearsWithHealthAboveThree();
+        List<Animal> GetAnimals(AnimalType animalType, AnimalState animalState);
     }
 }
