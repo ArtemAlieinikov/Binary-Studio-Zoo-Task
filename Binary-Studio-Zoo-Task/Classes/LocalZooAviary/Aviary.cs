09afa61a4eb60028533740e29167d860a3331c80
ArtemAlieinikov <artemalieinikov@gmail.com>
2017-05-30 02:28:18 +0300

    New methods of IZooRepository has been realized according to task.

--- Binary-Studio-Zoo-Task/Classes/LocalZooAviary/Aviary.cs
+++ Binary-Studio-Zoo-Task/Classes/LocalZooAviary/Aviary.cs
@@ -28,6 +28,28 @@ namespace Binary_Studio_Zoo_Task.Classes.LocalZooAviary
             }
         }
 
+        public bool DeleteAnimalByName(string animalName)
+        {
+            lock (lockObject)
+            {
+                Animal removedAnimal = animals.Where(x => x.AnimalName == animalName).FirstOrDefault();
+                if (removedAnimal != null)
+                {
+                    animals.Remove(removedAnimal);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public double GetAvarageHealth()
+        {
+            lock (lockObject)
+            {
+                return animals.Average(x => x.HealPoints);
+            }
+        }
+
         public Animal GetRandomAnimal()
         {
             int numberOfAnimals = animals.Count;
@@ -50,7 +72,7 @@ namespace Binary_Studio_Zoo_Task.Classes.LocalZooAviary
             }
         }
 
-        public Animal GetAnimalByName(string animalName)
+        public Animal GetAnimal(string animalName)
         {
             lock (lockObject)
             {
@@ -58,18 +80,81 @@ namespace Binary_Studio_Zoo_Task.Classes.LocalZooAviary
             }
         }
 
-        public bool DeleteAnimalByName(string animalName)
+        public Animal GetAnimal(AnimalType animalType, string animalName)
         {
             lock (lockObject)
             {
-                Animal removedAnimal = animals.Where(x => x.AnimalName == animalName).FirstOrDefault();
-                if (removedAnimal != null)
+                return animals.Where(x => (x.AnimalType == animalType) && (x.AnimalName.ToLower() == animalName.ToLower()))
+                    .FirstOrDefault();
+            }
+        }
+
+        public IEnumerable<IGrouping<AnimalType, Animal>> GetAnimalGroups()
+        {
+            lock (lockObject)
+            {
+                return animals.GroupBy(x => x.AnimalType);
+            }
+        }
+
+        public Dictionary<AnimalType, Animal> GetHealthiestAnimalOnGroup()
+        {
+            Dictionary<AnimalType, Animal> result = new Dictionary<AnimalType, Animal>();
+
+            lock (lockObject)
+            {
+                var animalGroup = animals.GroupBy(x => x.AnimalType);
+
+                foreach (var group in animalGroup)
                 {
-                    animals.Remove(removedAnimal);
-                    return true;
+                    result.Add(group.Key, group.OrderByDescending(x => x.HealPoints).FirstOrDefault());
                 }
             }
-            return false;
+
+            return result;
+        }
+
+        public Dictionary<AnimalType, int> GetNumberOfDethInGroup()
+        {
+            Dictionary<AnimalType, int> result = new Dictionary<AnimalType, int>();
+
+            lock (lockObject)
+            {
+                var animalGroup = animals.GroupBy(x => x.AnimalType);
+
+                foreach (var group in animalGroup)
+                {
+                    result.Add(group.Key, group.Count(x => x.AnimalState == AnimalState.dead));
+                }
+            }
+
+            return result;
+        }
+
+        public List<Animal> GetAnimalsWithMaxAndMinHealth()
+        {
+            lock (lockObject)
+            {
+                return animals.OrderByDescending(x => x.HealPoints).Take(1).Union(animals.OrderBy(x => x.HealPoints).Take(1))
+                    .ToList();
+            }
+        }
+
+        public List<string> GetNamesOfHangryAnimals()
+        {
+            lock (lockObject)
+            {
+                return animals.Where(x => x.AnimalState == AnimalState.hungry).Select(x => x.AnimalName).ToList();
+            }
+        }
+
+        public List<Animal> GetWolfsAndBearsWithHealthAboveThree()
+        {
+            lock (lockObject)
+            {
+                return animals.Where(x => ((x.AnimalType == AnimalType.bear) || (x.AnimalType == AnimalType.wolf)) && x.HealPoints > 3)
+                    .ToList();
+            }
         }
 
         public List<Animal> GetAllAnimals()
@@ -80,7 +165,7 @@ namespace Binary_Studio_Zoo_Task.Classes.LocalZooAviary
             }
         }
 
-        public List<Animal> GetAnimalsByType(AnimalType animalType)
+        public List<Animal> GetAnimals(AnimalType animalType)
         {
             lock (lockObject)
             {
@@ -88,11 +173,19 @@ namespace Binary_Studio_Zoo_Task.Classes.LocalZooAviary
             }
         }
 
-        public List<Animal> GetAnimalByState(AnimalState animalState)
+        public List<Animal> GetAnimals(AnimalState animalState)
         {
             lock (lockObject)
             {
                 return animals.Where(x => x.AnimalState == animalState).ToList();
+            }
+        }
+
+        public List<Animal> GetAnimals(AnimalType animalType, AnimalState animalState)
+        {
+            lock (lockObject)
+            {
+                return animals.Where(x => (x.AnimalType == animalType) && (x.AnimalState == animalState)).ToList();
             }
         }
     }
