54b1a02f1b05554e3abc0dc8ed665bef50e548be
ArtemAlieinikov <artemalieinikov@gmail.com>
2017-05-30 02:51:47 +0300

    Support information has been added.

--- Binary-Studio-Zoo-Task/Program.cs
+++ Binary-Studio-Zoo-Task/Program.cs
@@ -11,6 +11,20 @@ namespace Binary_Studio_Zoo_Task
     {
         static void Main(string[] args)
         {
+            #region helps
+            //show_groups - получить животных, сгруппированных по виду, 
+            //show_full - получить животных, с состоянием full,
+            //show_tiger_ill - получить всех тигров, которые больны,
+            //show_elephant_name_Robi - получить слона с кличкой Robi
+            //show_hungry_names - получить список всех кличек животных, которые голодны,
+            //show_healthiest - получить самых здоровых животных каждого вида,
+            //show_numberOf_death - получить количество мертвых животных каждого вида,
+            //show_wolfs_bears - получить всех волков и медведей, у которых здоровье выше 3,
+            //show_maxMin_health - получить животных с макс.и мин. здоровьем,
+            //show_avarage_health - получить среднее количество здоровья.
+            #endregion
+
+            #region start commands
             //string[] startCommands =
             //{
             //    "show_groups",
@@ -24,6 +38,7 @@ namespace Binary_Studio_Zoo_Task
             //    "show_maxMin_health",
             //    "show_avarage_health",
             //};
+            #endregion
 
             new Menu().Run(null);
         }
